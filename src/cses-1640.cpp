fdd86338d3198ce9624afd61b9612d144e672d31
Zap <67962871+ziap@users.noreply.github.com>
2024-05-06 09:04:26 +0700

    Update CSES problem 1640

--- src/cses-1640.cpp
+++ src/cses-1640.cpp
@@ -1,21 +1,36 @@
 #include <bits/stdc++.h>
 
+constexpr uint32_t EXP = 26;
+constexpr uint32_t N = 1 << EXP;
+constexpr uint32_t mask = N - 1;
+struct {
+  uint32_t k;
+  uint32_t v;
+} hash[N] = {};
+
+constexpr uint32_t MUL = 747796405;
+
 int main(void) {
-  int n, x;
+  uint32_t n, x;
   std::cin >> n >> x;
-
-  std::unordered_map<int, int> m;
-
-  for (int i = 1; i <= n; ++i) {
-    int tmp;
+ 
+  for (uint32_t i = 0; i < n; ++i) {
+    uint32_t tmp;
     std::cin >> tmp;
-    auto found = m.find(x - tmp);
-    if (found != m.end()) {
-      std::cout << found->second << ' ' << i << '\n';
-      return 0;
+    uint32_t h = ((tmp * MUL) ^ ((x - tmp) * MUL));
+ 
+    bool insert = true;
+    for (h >>= (32 - EXP); hash[h].k != 0; h = (h + 1) & mask) {
+      if (hash[h].v == x - tmp) {
+        std::cout << hash[h].k << ' ' << i + 1 << '\n';
+        return 0;
+      }
+      if (hash[h].v == tmp) insert = false;
     }
-    m[tmp] = i;
+    
+    if (insert) hash[h] = {i + 1, tmp};
   }
-
+ 
   std::cout << "IMPOSSIBLE\n";
+  return 0;
 }
